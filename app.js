54494a3ef7fcb3bc42ea61d4e8c35fc3be9d3320
John B Garcia <johngarcia9110@gmail.com>
2016-10-24 20:04:28 -0400

    created home page layout and testing css

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var http = require('http').Server(app);
+var sassMiddleware = require('node-sass-middleware');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public/sass/'),
+  dest:path.join(__dirname, 'public'),
+  debug: true,
+  indentedSyntax: true,
+  outputStyle: 'expanded',
+  prefix: '/stylesheets'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+http.listen(3000, function(){
+  console.log('listen on port: 3000');
+});
